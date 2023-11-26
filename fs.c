21cb0d615eb6804d95122f3d9306029b809d8718
OJH <wjdghks728@naver.com>
2023-11-27 00:28:30 +0900

    modify itrunc() to free addrs[0] ~ [12], test 50000 success

--- fs.c
+++ fs.c
@@ -376,7 +376,7 @@ bmap(struct inode *ip, uint bn)
   struct buf *bp;
 
   uint tmp = bn;
-  cprintf("tmp : %d\n", tmp);
+  /*
   if (tmp == 30) {
 	  if ((addr = ip->addrs[12]) ==0){
 		cprintf("addr 0x%x\n", addr);
@@ -385,6 +385,7 @@ bmap(struct inode *ip, uint bn)
 	  }
 	  return addr;
   }
+  */
 
   if(bn < NDIRECT){  // if bn'th block is direct block
     if((addr = ip->addrs[bn]) == 0) { // if n'th block in addrs is null 
@@ -511,7 +512,8 @@ bmap(struct inode *ip, uint bn)
 static void
 itrunc(struct inode *ip)
 {
-	/*
+  cprintf("In itrunc\n");
+
   int i, j;
   struct buf *bp;
   struct buf *bp2;
@@ -519,91 +521,6 @@ itrunc(struct inode *ip)
   uint *a;
   uint *a2;
   uint *a3;
-
-  for(i = 0; i < NDIRECT; i++){  // addrs[0] ~ addrs[5]
-    if(ip->addrs[i]){  // if exists, free 
-      bfree(ip->dev, ip->addrs[i]);
-      ip->addrs[i] = 0;  // set null
-    }
-  }
-
-  for (int i=1; i<=4; ++i) {  // addrs[6] ~ addrs[9]
-    if(ip->addrs[5 + i]){
-      bp = bread(ip->dev, ip->addrs[5 + i]);
-      a = (uint*)bp->data;
-      for(j = 0; j < 128; j++){  
-        if(a[j])
-          bfree(ip->dev, a[j]); // free in lv 1
-      }
-      brelse(bp);
-      bfree(ip->dev, ip->addrs[5 + i]); // free in addrs
-      ip->addrs[5 + i] = 0;
-    }
-  }
-
-  for (int i=1; i<=2; ++i) {  // addrs[10] ~ addrs[11]
-    if (ip->addrs[9 + i]) {
-	  bp = bread(ip->dev, ip->addrs[9 + i]);
-	  a = (uint *)bp->data;
-
-      for (int j=0; j<128; ++j){  
-        if (a[j]) {
-		  bp2 = bread(ip->dev, a[j]);
-		  a2 = (uint *)bp2->data;
-
-		  for (int k=0; k<128; ++k) {
-		    if (a2[k])
-			  bfree(ip->dev, a2[k]);
-		  }
-
-		  brelse(bp2);
-		  bfree(ip->dev, a[j]);
-		}
-      }
-
-	  brelse(bp);
-	  bfree(ip->dev, ip->addrs[9 + i]);
-	  ip->addrs[9 + i] = 0;
-	}
-  }
-
-  if (ip->addrs[12]) {  // addrs[12]
-	bp = bread(ip->dev, ip->addrs[12]);
-	a = (uint *)bp->data;
-
-	for (int i=0; i<128; ++i) {
-	  if (a[i]) {
-		bp2 = bread(ip->dev, a[i]);
-		a2 = (uint *)bp2->data;
-
-		for (int j=0; j<128; ++j) {
-		  if (a2[j]) {
-			bp3 = bread(ip->dev, a2[j]);
-			a3 = (uint *)bp3->data;
-
-			for (int k=0; k<128; ++k) {
-			  if (a3[k]) {
-				bfree(ip->dev, a3[k]);
-			  }
-			}
-			brelse(bp3);
-			bfree(ip->dev, a2[j]);
-		  }
-		}
-		brelse(bp2);
-		bfree(ip->dev, a[i]);
-	  }
-	}
-	brelse(bp);
-	bfree(ip->dev, ip->addrs[12]);
-	ip->addrs[12] = 0;
-  }
-*/
-
-	cprintf("In itrunc\n");
-	int i, j;
-  struct buf *bp;
-  uint *a;
 
   for(i = 0; i < NDIRECT; i++){
 	  cprintf("for ip->addrs[%d]\n", i);
@@ -631,24 +548,73 @@ itrunc(struct inode *ip)
     }
   }
 
-  /*
-  if(ip->addrs[NDIRECT]){
-	  cprintf("for ip->addrs[%d]\n", NDIRECT);
-    bp = bread(ip->dev, ip->addrs[NDIRECT]);
-    a = (uint*)bp->data;
-    for(j = 0; j < NINDIRECT; j++){
-	  cprintf("for ip->addrs[%d]->[%d]\n", NDIRECT, j);
-      if(a[j]) {
-	    cprintf("free ip->addrs[%d]->[%d]\n", NDIRECT, j);
-        bfree(ip->dev, a[j]);
-	  }
-    }
-    brelse(bp);
-    bfree(ip->dev, ip->addrs[NDIRECT]);
-    ip->addrs[NDIRECT] = 0;
-  }
-  */
+  for (int i=1; i<=2; ++i) {  // addrs[10] ~ addrs[11]
+	cprintf("for ip->addrs[%d]\n", 9+i);
+    if (ip->addrs[9 + i]) {
+	  bp = bread(ip->dev, ip->addrs[9 + i]);
+	  a = (uint *)bp->data;
 
+      for (int j=0; j<128; ++j){  
+		cprintf("for ip->addrs[%d]->[%d]\n", 9+i, j);
+        if (a[j]) {
+		  bp2 = bread(ip->dev, a[j]);
+		  a2 = (uint *)bp2->data;
+
+		  for (int k=0; k<128; ++k) {
+			cprintf("for ip->addrs[%d]->[%d]->[%d]\n", 9+i, j, k);
+		    if (a2[k]) {
+			  bfree(ip->dev, a2[k]);
+			  cprintf("free ip->addrs[%d]->[%d]->[%d]\n", 9+i, j, k);
+			}
+		  }
+
+		  brelse(bp2);
+		  bfree(ip->dev, a[j]);
+		}
+      }
+
+	  brelse(bp);
+	  bfree(ip->dev, ip->addrs[9 + i]);
+	  ip->addrs[9 + i] = 0;
+	}
+  }
+
+  if (ip->addrs[12]) {  // addrs[12]
+	cprintf("for ip->addrs[12]\n");
+	bp = bread(ip->dev, ip->addrs[12]);
+	a = (uint *)bp->data;
+
+	for (int i=0; i<128; ++i) {
+	  cprintf("for ip->addrs[12]->[%d]\n", i);
+	  if (a[i]) {
+		bp2 = bread(ip->dev, a[i]);
+		a2 = (uint *)bp2->data;
+
+		for (int j=0; j<128; ++j) {
+		  cprintf("for ip->addrs[12]->[%d]->[%d]\n", i, j);
+		  if (a2[j]) {
+			bp3 = bread(ip->dev, a2[j]);
+			a3 = (uint *)bp3->data;
+
+			for (int k=0; k<128; ++k) {
+			  cprintf("for ip->addrs[12]->[%d]->[%d]->[%d]\n", i, j, k);
+			  if (a3[k]) {
+			    cprintf("free ip->addrs[12]->[%d]->[%d]->[%d]\n", i, j, k);
+				bfree(ip->dev, a3[k]);
+			  }
+			}
+			brelse(bp3);
+			bfree(ip->dev, a2[j]);
+		  }
+		}
+		brelse(bp2);
+		bfree(ip->dev, a[i]);
+	  }
+	}
+	brelse(bp);
+	bfree(ip->dev, ip->addrs[12]);
+	ip->addrs[12] = 0;
+  }
 
   cprintf("itrunc end\n");
 
